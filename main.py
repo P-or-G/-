9f456b9406648473453926513fb888a13bf15a78
Fedor Gornostaev <102575921+P-or-G@users.noreply.github.com>
2022-10-23 18:41:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QLabel, QFileDialog
 from PyQt5.uic import loadUi
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QColor, QPixmap
+from PyQt5 import uic
 import sys
 
 
-class Example(QMainWindow):
+class Main(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        loadUi('Главное окно.ui', self)
+        self.pushButton_2.clicked.connect(self.pole)
 
-    def initUI(self):
-        loadUi('Поле.ui', self)
+
+
+    def pole(self):
+        global new_window
+        new_window = uic.loadUi("Поле.ui")
+        new_window.setWindowTitle("New form")
+        new_window.show()
+
 
 
 def except_hook(cls, exception, traceback):
@@ -19,7 +27,7 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Main()
     ex.show()
     sys.excepthook = except_hook
     sys.exit(app.exec())
